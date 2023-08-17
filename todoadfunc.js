fcc1bc432a98102b392e1f330820a1b5ab70dc87
cafferti <saulkumven@gmail.com>
2023-08-17 10:56:25 +0100

    used foreach to replace the loop in my todolist

--- todoadfunc.js
+++ todoadfunc.js
@@ -1,40 +1,32 @@
-const todolist = JSON.parse(localStorage.getItem('todolist'));
+const todolist = JSON.parse(localStorage.getItem("todolist"));
 
 displaytodolist();
 
-function displaytodolist(){
-  
-let todolistHtml = ``
+function displaytodolist() {
+  let todolistHtml = ``;
 
-todolist.forEach(function(todoobject, index) {
-  const task = todoobject.name;
-  const duedate = todoobject.duedate;
-  const html = `
+  todolist.forEach(function (todoobject, index) {
+    const task = todoobject.name;
+    const duedate = todoobject.duedate;
+    const html = `
     <div>${task}</div>
     <div>${duedate}</div>
     <button onclick="todolist.splice(${index}, 1); displaytodolist();">Delete</button>
   `;
-  todolistHtml += html;
-});
-localStorage.setItem(`todolist`, JSON.stringify(todolist))
-document.querySelector(`.js-todo`).innerHTML = todolistHtml
+    todolistHtml += html;
+  });
+  localStorage.setItem(`todolist`, JSON.stringify(todolist));
+  document.querySelector(`.js-todo`).innerHTML = todolistHtml;
+}
 
-
-};
-
-
-
-function addtodo(){
-  
-const inpElement = document.querySelector(`.js-input`) 
-const name =  inpElement.value;
-const dateinp =  document.querySelector(`.js-duedate`);
-const duedate = dateinp.value;
-todolist.push({name: name,
-              duedate: duedate
-          })
-console.log(todolist)
-inpElement.value =  ""
-dateinp.value =  ""
-displaytodolist();
-};
+function addtodo() {
+  const inpElement = document.querySelector(`.js-input`);
+  const name = inpElement.value;
+  const dateinp = document.querySelector(`.js-duedate`);
+  const duedate = dateinp.value;
+  todolist.push({ name: name, duedate: duedate });
+  console.log(todolist);
+  inpElement.value = "";
+  dateinp.value = "";
+  displaytodolist();
+}
